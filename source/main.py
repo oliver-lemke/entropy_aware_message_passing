cbc6a88b25c5ffb9f7fd8f51e6854ec64e832228
artiom <artiomgsp@gmail.com>
2023-11-27 19:02:11 +0100

    adding basic dirichlet energy test structure

--- source/main.py
+++ source/main.py
@@ -1,9 +1,12 @@
 from trainer.base_trainer import BaseTrainer
+from tester.base_tester import BaseTester
 
 
 def main():
-    agent = BaseTrainer()
-    agent.train()
+    # agent = BaseTrainer()
+    # agent.train()
+    tester = BaseTester()
+    print(tester.test())
 
 
 if __name__ == "__main__":
