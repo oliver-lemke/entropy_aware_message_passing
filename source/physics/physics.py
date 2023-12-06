734b1a6ebd578dc7628e34797e8e5b4ee3a8261d
oliver_lemke <lemke.oliver@icloud.com>
2023-12-06 22:48:00 +0100

    extract physics to physics/ folder

--- source/physics/physics.py
+++ source/physics/physics.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import torch
 
 
 class Entropy:
-    def __init__(self, T, A=0):
+    def __init__(self, A=0):
         """Class to handle the entropy of a given Graph
 
         Args:
@@ -12,12 +14,11 @@ class Entropy:
         """
 
         # TODO: implement everything with sparse matrices. Don't use adjacency matrix, but edge_index
+        self.A = None
+        self.L = None
 
         # adjacency matrix
         self.update_adj(A)
-
-        # temperature
-        self.T = T
 
     def update_adj(self, A):
         """Update the adjacency matrix
@@ -32,19 +33,41 @@ class Entropy:
         D = torch.diag(torch.sum(A, dim=0))
         self.L = D + A
 
-    def entropy(self, X):
+    def entropy(self, X, temperature):
         """Compute the entropy of the graph embedding X
 
         Returns:
             float: entropy of X
         """
 
-        distribution = self.boltzmann_distribution(X)
+        distribution = self.boltzmann_distribution(X, temperature)
+
         S = -torch.sum(distribution * torch.log(distribution))
 
         return S
 
-    def dirichlet_energy(self, X):
+    def total_dirichlet_energy(self, X):
+        """Compute the total dirichlet energy of the graph embedding X
+
+        Args:
+            X (torch.tensor, shape NxN): graph embedding
+        """
+
+        return torch.mean(self.dirichlet_energy(X))
+
+    def compute_energy_normalization(self, dim: int):
+        """
+        Compute normalization for each nodes dirichlet energy.
+        """
+
+        # compute degree of each node
+        degrees = torch.sum(self.A, dim=1)
+
+        # compute normalization
+        norm = 1 / torch.sqrt(degrees * dim)
+        return norm.unsqueeze(-1)
+
+    def dirichlet_energy(self, X, normalize_energies: bool = False):
         """Comute Dirichlet Energie for graph embedding X
 
         Returns:
@@ -57,48 +80,72 @@ class Entropy:
 
         energies = 1 / 2 * (res1 - 2 * res2 + res3)
 
-        # FIXME there are sometimes negative energies, which is mathematically impossible?!
-        # But, we should use sparse matrices anyways. Maybe then, if we just do ordinary matrix
-        # multiplication, we don't have this problem anymore?
-        # also, values are complete garbage
+        if normalize_energies:
+            energies = energies * self.compute_energy_normalization(energies.shape[0])
+
+        # (because of numerical errors ??) some energies are an epsilon negative. Clamp those.
+        # FIXME still, not sure why this is happening. We should see whether this issue goes away
+        # with sparse matrices
         energies.clamp(min=1e-10)
 
-        # res1 = torch.einsum("jk,ij,jk->i", X, self.L, X)
-        # res2 = torch.einsum("ik,ij,jk->i", X, self.A, X)
-        # energies = 1/2*(res1 - 2 * res2)
+        # print(f"energies: {torch.sum(~torch.isfinite(energies))}")
 
-        # abbreviate this for loop using torch.einsum
         return energies
 
-    def boltzmann_distribution(self, X):
+    def boltzmann_distribution(self, X, temperature):
         """Compute Boltzmann distribution
 
         Returns:
             torch.tensor, shape N: Boltzmann distribution given energies and temperature
         """
 
-        # FIXME: right now, normalize with shape s.t. energies are non-zero
         energies = self.dirichlet_energy(X)
 
         # return softmax of energies scaled by temperature
-        distribution = torch.softmax(-energies / self.T, dim=0)
+        # adding an epsilon is a hack to avoid log(0) = -inf.
+        # x*ln(x) goes to 0 as x goes to 0, so this is okay
+        distribution = torch.softmax(-energies / temperature, dim=0) + 1e-10
 
-        # this is a hack to avoid log(0) = -inf. x*ln(x) goes to 0 as x goes to 0, so this is okay
-        distribution += 1e-10
         return distribution
 
-    def Pbar(self, X):
-        P = self.boltzmann_distribution(X)
-        S = self.entropy(X)
+    def Pbar(self, X, temperature):
+        P = self.boltzmann_distribution(X, temperature)
+        S = self.entropy(X, temperature)
         P_bar = P * (S + torch.log(P))
 
         return P_bar
 
-    def gradient_entropy(self, X):
-        P_bar = self.Pbar(X)
+    def gradient_entropy(self, X, temperature: float, normalize_energies: bool = False):
+        P_bar = self.Pbar(X, temperature)
         res1 = torch.einsum("ij,ik,i->ik", self.A, X, P_bar)
         res2 = torch.einsum("ij,ik,j->ik", self.A, X, P_bar)
         res3 = torch.einsum("ij,jk,i->ik", self.A, X, P_bar)
         res4 = torch.einsum("ij,jk,j->ik", self.A, X, P_bar)
 
-        return 1 / self.T * (res1 + res2 - res3 - res4)
+        result = 1 / temperature * (res1 + res2 - res3 - res4)
+
+        if normalize_energies:
+            result = result * self.compute_energy_normalization(result.shape[0])
+
+        """
+        print(result)
+        test = self.Pbar(X)
+
+        res = []
+        for i in range(X.shape[0]):
+            sum = 0
+            for j in range(X.shape[0]):
+                contrib = self.energy_normalization[i]*(
+                    self.A[i, j] * X[i] * test[i]
+                    + self.A[i, j] * X[i] * test[j]
+                    - self.A[i, j] * X[j] * test[i]
+                    - self.A[i, j] * X[j] * test[j]
+                )
+                sum += contrib
+            res.append(sum)
+
+            print(1 / self.temperature * sum)
+
+        """
+
+        return result
