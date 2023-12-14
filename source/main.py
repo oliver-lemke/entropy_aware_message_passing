c8670294850a18b2204c9aa37236d44668be0d80
artiom <artiomgsp@gmail.com>
2023-12-14 15:44:58 +0100

    added long range dataset

--- source/main.py
+++ source/main.py
@@ -1,5 +1,6 @@
 from tester.base_tester import BaseTester
 from trainer.base_trainer import BaseTrainer
+from trainer.multi_graph_trainer import MultiGraphTrainer
 from utils.config import Config
 
 config = Config()
@@ -7,8 +8,13 @@ config = Config()
 
 def main():
     if config["run_type"] == "train":
-        agent = BaseTrainer()
-        agent.train()
+        if config["multi_graph_dataset"]:
+            agent = MultiGraphTrainer()
+            agent.train()            
+        else:
+            agent = BaseTrainer()
+            agent.train()
+            
     elif config["run_type"] == "test":
         tester = BaseTester()
         tester.test_energy_per_layer()
