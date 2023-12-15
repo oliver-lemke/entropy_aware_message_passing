e91436f669d9b539b943101f5f229d0c09634478
Artiom Gesp <argesp@eu-login-45.euler.ethz.ch>
2023-12-15 11:20:10 +0100

    added validation

--- source/trainer/multi_graph_trainer.py
+++ source/trainer/multi_graph_trainer.py
@@ -81,7 +81,7 @@ class MultiGraphTrainer(BaseTrainer):
             batch_size=config["hyperparameters"]["train"]["batch_size"],
         )
 
-    def step(self, data):
+    def train_step(self, data):
         self._step += 1
         # training step
         self.model.train()
@@ -96,17 +96,33 @@ class MultiGraphTrainer(BaseTrainer):
 
         with torch.no_grad():
             self.model.clamp_learnables()
-        self._log(data, pred, loss, intermediate_representations)
+        self._log_train(data, pred, loss, intermediate_representations)
+    
+    def val_step(self, data):
+        # training step
+        self.model.train()
+        data = data.to(config["device"])
+
+        # gradient descent
+        self.optimizer.zero_grad()
+        pred, intermediate_representations = self.model(data)
+        loss = self.loss(pred, data.y)
+
+        with torch.no_grad():
+            self.model.clamp_learnables()
+        self._log(data, pred, loss, intermediate_representations, False)
 
     def one_epoch(self):
         logger.info(
             f"Training epoch {self._epoch} / {config['hyperparameters']['train']['epochs']}"
         )
         for data in self.train_loader:
-            self.step(data)
+            self.train_step(data)
+        for data in self.val_loader:
+            self.val_step(data)
         self.scheduler.step()
 
-    def _log(self, data, pred, loss, int_reps):
+    def _log_train(self, data, pred, loss, int_reps):
         # ENtropy Object
         A = torch_geometric.utils.to_dense_adj(data.edge_index).squeeze()
         entropy = Entropy(A=A)
@@ -116,12 +132,92 @@ class MultiGraphTrainer(BaseTrainer):
             # normal metrics
             train_metrics = metrics(pred, data.y, reduction="mean")
             train_metrics["total_loss"] = loss.item()
-            # val_metrics = metrics(
-            #     pred[data.val_mask], data.y[data.val_mask], reduction="mean"
-            # )
-            # # validation loss
-            # val_loss = self.loss(pred[data.val_mask], data.y[data.val_mask])
-            # val_metrics["total_loss"] = val_loss.item()
-            # if val_loss < self.prev_val_loss:
-            #     self.prev_val_loss = val_loss
-            #     self.best_checkpoint = True
+         # prepare for logging
+        scalar_metrics = add_prefix_to_dict(train_metrics, "train/")
+
+        other = {}
+        if config["wandb"]["extended"]:
+            with torch.no_grad():
+                total_metrics = {}
+                energies, entropies = [], []
+                # calculate energy and entropy from intermediate representations
+                for layer, int_rep in int_reps.items():
+                    energy_metric = entropy.dirichlet_energy(int_rep).mean()
+                    entropy_metric = entropy.entropy(int_rep, 1.0)
+                    energies.append(energy_metric)
+                    entropies.append(entropy_metric)
+                    if isinstance(layer, int):
+                        total_metrics[
+                            f"energy_over_epoch/layer{layer:04d}"
+                        ] = energy_metric
+                        total_metrics[
+                            f"entropy_over_epoch/layer{layer:04d}"
+                        ] = entropy_metric
+                    else:
+                        total_metrics[f"energy_over_epoch/layer{layer}"] = energy_metric
+                        total_metrics[
+                            f"entropy_over_epoch/layer{layer}"
+                        ] = entropy_metric
+
+                # plot over layers
+                table_data = [
+                    [layer, energy.item(), entropy.item()]
+                    for layer, (energy, entropy) in enumerate(zip(energies, entropies))
+                ]
+                # Create a wandb Table
+                energy_entropy_table = wandb.Table(
+                    data=table_data, columns=["Layer", "Energy", "Entropy"]
+                )
+                # Log the table as line plots
+                other[f"energy_over_layers/step{self._step:04d}"] = wandb.plot.line(
+                    table=energy_entropy_table,
+                    x="Layer",
+                    y="Energy",
+                    title=f"energy_over_layers/step{self._step:04d}",
+                )
+                other[f"entropy_over_layers/step{self._step:04d}"] = wandb.plot.line(
+                    table=energy_entropy_table,
+                    x="Layer",
+                    y="Entropy",
+                    title=f"entropy_over_layers/step{self._step:04d}",
+                )
+
+                # histogram of last
+                last_layer = max([k for k in int_reps if isinstance(k, int)])
+                energy_metric = entropy.dirichlet_energy(int_reps[last_layer])
+                other[f"histograms/final_energy_step{self._step}"] = wandb.Histogram(
+                    energy_metric.cpu().numpy()
+                )
+                total_metrics = add_prefix_to_dict(total_metrics, "total/")
+                scalar_metrics = combine_dicts(**scalar_metrics, **total_metrics)
+
+        self._log_all(scalar_metrics=scalar_metrics, other_wandb=other)
+
+        def _log_val(self, data, pred, loss, int_reps):
+        # ENtropy Object
+        A = torch_geometric.utils.to_dense_adj(data.edge_index).squeeze()
+        entropy = Entropy(A=A)
+        # metrics
+        self.model.eval()
+        with torch.no_grad():
+            # normal metrics
+            val_metrics = metrics(
+                pred, data.y, reduction="mean"
+            )
+            # validation loss
+            val_metrics["total_loss"] = loss.item()
+            if val_loss < self.prev_val_loss:
+                self.prev_val_loss = val_loss
+                self.best_checkpoint = True
+         # prepare for logging
+        val_metrics = add_prefix_to_dict(val_metrics, "val/")
+
+        self._log_all(scalar_metrics=val_metrics, other_wandb={})
+
+    def one_epoch(self):
+        logger.info(
+            f"Training epoch {self._epoch} / {config['hyperparameters']['train']['epochs']}"
+        )
+        for data in self.loader:
+            self.step(data)
+        self.scheduler.step()
